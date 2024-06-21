8d50138df070b00dfa93f47f117d3ba8a8bb63ed
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-06-21 21:07:55 +0300

    task 2 and 3 ready

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+const liEl = ingredients
+  .map((item) => `<li class='item'>${item}</li>`)
+  .join("");
+
+ulEl.insertAdjacentHTML("beforeend", liEl);
