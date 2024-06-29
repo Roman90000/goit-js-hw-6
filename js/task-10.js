ac505136de30666df1268572782e6cedb9802b6c
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-06-30 00:20:56 +0300

    task 10 ready

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const divBox = document.querySelector("#boxes");
+
+input.addEventListener("change", onInput);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function onInput(evt) {
+  const inputValue = evt.currentTarget.value;
+  createBoxes(inputValue);
+}
+
+function createBoxes(amount) {
+  for (let i = 0, b = 30; i < amount; i++, b += 10) {
+    let createBox = document.createElement("div");
+    createBox.style.width = b + "px";
+    createBox.style.height = b + "px";
+    createBox.style.margin = "10px";
+    createBox.style.backgroundColor = "tomato";
+
+    divBox.insertAdjacentElement("beforeend", createBox);
+  }
+}
+
+function destroyBoxes() {
+  divBox.innerHTML = "";
+  input.value = "";
+}
