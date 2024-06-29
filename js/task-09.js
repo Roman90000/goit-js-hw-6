d3c6b13ccbe254cba6509d968334154dd34febb8
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-06-29 21:13:48 +0300

    task 9 ready

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const cpan = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  cpan.textContent = color;
+  body.style.backgroundColor = color;
+}
