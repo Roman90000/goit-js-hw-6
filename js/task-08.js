255b7a9d3e9bda90213e4adb11d2142b25a2fdf7
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2024-06-29 21:01:41 +0300

    task 8 ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onForm);
+
+function onForm(evt) {
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+
+  const data = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  if (!data.email || !data.password) {
+    alert("Все поля должны быть заполнены.");
+  }
+  evt.currentTarget.reset();
+}
